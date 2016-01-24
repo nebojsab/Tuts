e42c22c3abf8445bd583c8fb243595eece0ad9df
nebojsab <nebojsa.brnjasevic@gmail.com>
2016-01-24 15:18:50 +0100

    JavaScript - jQuery Tuts

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 'use strict';
 
-$(function(){
+$(function () {
 
     //configuration
     var width = 1140;
     var animationSpeed = 1000;
-    var pause = 1000;
+    var pause = 3000;
     var currentSlide = 1;
 
     //cache DOM
@@ -13,14 +13,26 @@ $(function(){
     var $slideContainer = $slider.find('.slides');
     var $slides = $slideContainer.find('.slide');
 
-    setInterval(function() {
-       $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function(){
-            currentSlide++;
-           if (currentSlide === $slides.length){
-           currentSlide = 1;
-           $slideContainer.css('margin-left', 0);
-           }
-       });
-    }, pause);
+    var interval;
 
+    function startSlider() {
+        interval = setInterval(function () {
+            $slideContainer.animate({'margin-left': '-=' + width}, animationSpeed, function () {
+                currentSlide++;
+                if (currentSlide === $slides.length) {
+                    currentSlide = 1;
+                    $slideContainer.css('margin-left', 0);
+                }
+            });
+        }, pause);
+    }
+
+    function pauseSlider(){
+        clearInterval(interval);
+    }
+
+    $slider.on('mouseenter', pauseSlider).on('mouseleave', startSlider);
+
+    startSlider();
+    
 });
